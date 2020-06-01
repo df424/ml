2f0d765633af34379019cab17b143069c8ae2b33
df424 <d.l.flanagan01@gmail.com>
2020-05-31 21:01:58 -0400

    implemented pooling
    1.) Forward pooling complete
    2.) Backward pooling complete
    3.) Updated YaleFaceResampleVectorizor to work with convolutional nets.

--- data/vectorizers/yale_face_resample_vectorizer.py
+++ data/vectorizers/yale_face_resample_vectorizer.py
@@ -11,9 +11,11 @@ from ml.utility import label_map_2_one_hot, add_bias
 from tqdm.autonotebook import tqdm
 
 class YaleFaceResampleVectorizer(Vectorizer):
-    def __init__(self, add_bias=True):
+    def __init__(self, add_bias=True, flatten: bool = True, mode='concat'):
         self._label_map = {}
         self._add_bias = add_bias
+        self._mode = mode
+        self._flatten = flatten
 
     def index_vectorizer(self, reader: DatasetReader):
         X = []
@@ -22,9 +24,15 @@ class YaleFaceResampleVectorizer(Vectorizer):
             # assign it the next integer since its a 1-hot thing.
             if inst.y not in self._label_map:
                 self._label_map[inst.y] = len(self._label_map)
-            X.append(YaleFaceResampleVectorizer._flatten_and_resize_img(inst.x))
+            X.append(YaleFaceResampleVectorizer._flatten_and_resize_img(inst.x, self._flatten))
 
-        X = np.concatenate(X)
+        if self._mode == 'concat':
+            X = np.concatenate(X)
+        elif self._mode == 'array':
+            X = np.array(X)
+        else:
+            raise ValueError(f'MODE={self._mode} not supported.')
+
         self._means = X.mean(axis=0)
         self._std = X.std(axis=0)
 
@@ -40,7 +48,7 @@ class YaleFaceResampleVectorizer(Vectorizer):
             self._output_2_label[v] = k
 
     def vectorize_input(self, input: Any) -> np.ndarray:
-        x = YaleFaceResampleVectorizer._flatten_and_resize_img(input)
+        x = YaleFaceResampleVectorizer._flatten_and_resize_img(input, self._flatten)
         x_norm = (x-self._means)/self._std
         x_norm[self._feature_mask] = 0
         if self._add_bias:
@@ -62,6 +70,9 @@ class YaleFaceResampleVectorizer(Vectorizer):
         return 1600
 
     @staticmethod
-    def _flatten_and_resize_img(img):
-        return np.asarray(img.resize((40,40), resample=Image.LANCZOS)).reshape(1,-1)
+    def _flatten_and_resize_img(img, flatten: bool):
+        if flatten:
+            return np.asarray(img.resize((40,40), resample=Image.LANCZOS)).reshape(1,-1)
+        else:
+            return np.asarray(img.resize((40,40), resample=Image.LANCZOS))
 
--- models/convolution.py
+++ models/convolution.py
@@ -2,6 +2,7 @@
 from typing import Dict, Any, Tuple
 import numpy as np
 from scipy.signal import convolve2d
+import math
 
 from ml.models import Model
 from ml.modules.initializers import Initializer
@@ -65,24 +66,75 @@ class ConvolutionalLayer2D(Model):
 
 
 class PoolingLayer2D(Model):
-    def __init__(self, shape:Tuple[int, int], stride: int, method:str='max', mode:str='valid'):
+    def __init__(self, shape:Tuple[int, int], stride: Tuple[int, int], method:str='max', mode:str='valid'):
+        self._shape = shape
+        self._stride = stride
+        self._method = method
         self._mode = mode
-        self._parameters = {}
-        self._gradients = {}
+        self._cache = {}
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        if self._mode == 'valid':
-            pass
-        else:
-            raise ValueError(f'Unknown mode: {self._mode}')
+        # We know the size of our pooling layer from the size of the images and stride so we can.
+        # allocate spaces once.
+        n_output_rows = math.ceil(X.shape[2]/self._stride[0])
+        n_output_cols = math.ceil(X.shape[3]/self._stride[1])
+        pred = np.zeros((X.shape[0], X.shape[1], n_output_rows, n_output_cols))
+        # We need to keep track of the indices so we can back prop.
+        indices = np.zeros(X.shape)
+
+        # For each input image...
+        #print(X.shape, pred.shape)
+        for k in range(X.shape[0]):
+            # For each channel...
+            for n, x in enumerate(X[k]):
+                if self._mode == 'valid':
+                    # Iterate over rows.
+                    for i in range(0, n_output_rows):
+                        row_start = i*self._stride[0]
+                        row_end = row_start + self._shape[0]
+                        # Iterate over columns
+                        for j in range(0, n_output_cols):
+                            col_start = j*self._stride[1]
+                            col_end = col_start + self._shape[1]
+                            #print(row_start, row_end, col_start, col_end, self._shape, x.shape)
+                            #print(x[row_start:row_end, col_start:col_end])
+                            mi = np.unravel_index(np.argmax(x[row_start:row_end, col_start:col_end]), self._shape)
+                            #print(mi[0]+row_start, mi[1]+col_start, indices.shape)
+                            indices[k, n, mi[0]+row_start, mi[1]+col_start] = 1
+                            #print(f'PRED_INDICES: {k}, {n}, {i}, {j}')
+                            pred[k,n,i,j] = np.max(x[row_start:row_end, col_start:col_end])
+                else:
+                    raise ValueError(f'Unknown mode: {self._mode}')
+        # cache the indices so we can use them during backprop.
+        self._cache['I'] = indices
+        return pred
 
     def backward(self, loss: np.ndarray) -> None:
-        pass
+        # Get the indices from teh cache.
+        I = self._cache['I']
+        grads = np.zeros(I.shape)
+
+        n_output_rows = math.ceil(I.shape[2]/self._stride[0])
+        n_output_cols = math.ceil(I.shape[3]/self._stride[1])
+
+        for k in range(loss.shape[0]):
+            for n in range(loss.shape[1]):
+                for i in range(0, n_output_rows):
+                    row_start = i*self._stride[0]
+                    row_end = row_start + self._shape[0]
+                    # Iterate over columns
+                    for j in range(0, n_output_cols):
+                        col_start = j*self._stride[1]
+                        col_end = col_start + self._shape[1]
+                        #print(f'GRAD INDICES: k={k}, n={n}, row_start={row_start}, row_end={row_end}, col_start={col_start}, col_end={col_end}, i={i}, j={j}')
+                        grads[k, n, row_start:row_end, col_start:col_end] = loss[k,n,i,j]
+
+        return np.multiply(grads, I)
 
     @property
     def parameters(self) -> Dict[str, Any]:
-        return self._parameters
+        return {}
 
     @property
     def gradients(self) -> Dict[str, Any]:
-        return self._gradients
+        return {}
